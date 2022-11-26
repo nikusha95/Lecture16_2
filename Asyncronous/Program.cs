d11f5321e3ea25965871b48cc6796cb908f0c0fc
nikusha95 <nikasanaia11@yahoo.com>
2022-11-26 16:59:34 +0400

    init

---
+++ Asyncronous/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int r1 = Sum(1, 2);
+var r2 = await SumAsync(1, 2);
+
+Console.WriteLine(r1);
+Console.WriteLine(r2);
+
+int res = await SumResult(1, 2);
+
+Console.WriteLine(res);
+
+async Task<int> SumResult(int a, int b)
+{
+    return  await SumAsync(a, b);
+}
+Task<int> SumAsync(int a, int b)
+{
+    return Task.FromResult(a + b);
+}
+int Sum(int a, int b)
+{
+    return a + b;
+}
+
+void Print(string message){
+    Console.WriteLine(message);
+}
+
+async Task PrintAsync(string message)
+{
+    var r3 =await SumAsync(1, 5);
+    Console.WriteLine(message);
+}
